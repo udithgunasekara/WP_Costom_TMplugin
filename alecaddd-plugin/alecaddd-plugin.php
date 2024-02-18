5b7587db3e265c9595bae631477dca405a30de8a
Udith Gunasekara <135354133+udithgunasekara@users.noreply.github.com>
2024-02-18 23:13:25 +0530

    Update alecaddd-plugin.php

--- alecaddd-plugin/alecaddd-plugin.php
+++ alecaddd-plugin/alecaddd-plugin.php
@@ -4,12 +4,6 @@
  */
 /*
 Plugin Name: Alecaddd Plugin
-Plugin URI: http://alecaddd.com/plugin
-Description: This is my first attempt on writing a custom Plugin for this amazing tutorial series.
-Version: 1.0.0
-Author: Alessandro "Alecaddd" Castellani
-Author URI: http://alecaddd.com
-License: GPLv2 or later
 Text Domain: alecaddd-plugin
 */
 
